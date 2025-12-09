a02acf8c76ba0e928ecd3241b793dab8636b4208
tripicana277 <tripicana277@example.com>
2025-12-09 18:17:24 +0900

    Add requirements.txt for deployment

--- config/settings.py
+++ config/settings.py
@@ -106,18 +106,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Guitar4416!!',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
+        'NAME': '/home/site/wwwroot/db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#         # 'ENGINE': 'django.db.backends.postgresql',
+#         # 'NAME': 'postgres',
+#         # 'USER': 'postgres',
+#         # 'PASSWORD': 'Guitar4416!!',
+#         # 'HOST': 'localhost',
+#         # 'PORT': '5432',
+#     }
+# }
 
 
 
